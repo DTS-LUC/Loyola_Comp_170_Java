bcedd220d80d84cc5eb4ce25cf22f1746646ef66
Donald Stolz <dstolz@luc.edu>
2016-09-22 16:03:16 -0500

    Create dream.java

---
+++ dream.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class dream {
+	
+	public static void main(String[] args){
+		
+		Scanner input = new Scanner(System.in);
+		String color, food, animal, name;
+		
+		System.out.println("Hello, please enter your favorite color ");
+		color = input.nextString();
+		
+	  System.out.println("Hello, please enter your favorite food ");
+		food = input.nextString();
+		
+		System.out.println("Hello, please enter your favorite animal ");
+		animal = input.nextString();
+		
+		System.out.println("Hello, please enter the name of a relative or friend ");
+		name = input.nextString();
+	}
+}
