1b1ad438489ebc725a2d836c9ce9b3ab5abb72a0
Donald Stolz <groot@Harrisons-MacBook-Pro.local>
2016-09-27 15:35:43 -0500

    new commit

---
+++ Exam1.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Exam1 {
+
+	public static void main(String[] args) {
+
+		//Add scanner to allow the program to take input from the users keyboard
+		Scanner input = new Scanner(System.in); 
+		
+		//Ask the user for input
+		System.out.println("Hello, please enter your exam score");
+		//Store user input from scanner
+		double exam = input.nextDouble();
+		
+		//Create a variable for how the user did
+		String result;
+
+		//Compare the exam score, to find out how they did
+		if (exam >= 90) {
+			result = "Awesome!";
+		}
+		else if (exam >= 80) {
+			result = "Pretty good."
+		}
+		else if (exam > = 70) {
+			result = "Not so great..."
+		}
+		else {
+			result = "Awful..."
+		}
+
+		//Output the user score and result
+		System.out.println("My exam one score was %" + exam + " and I did " + result);
+	}
+	
+}
